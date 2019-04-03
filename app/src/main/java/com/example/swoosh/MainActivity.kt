ee2e99b5519de1f24d1994c2890da1ed58ab0fc0
Sumit <thunder11h@gmail.com>
2019-04-04 02:08:31 +0530

    Added Intent For Other Activity

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/swoosh/skillActivity.kt
+++ app/src/main/java/com/example/swoosh/skillActivity.kt
@@ -3,11 +3,11 @@ package com.example.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : BasicActivity() {
+class skillActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league)
+        setContentView(R.layout.activity_skill)
 
     }
 }
