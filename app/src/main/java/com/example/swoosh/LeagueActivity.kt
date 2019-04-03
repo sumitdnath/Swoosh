1d3c1f3676deee31124a3ab48540afe515cb9320
Sumit <thunder11h@gmail.com>
2019-04-04 01:18:03 +0530

    Added Intent For Other Activity

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -8,5 +8,6 @@ class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
     }
 }
